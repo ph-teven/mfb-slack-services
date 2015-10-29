7c5925a7eac11ad50fdda6c65863bc0e614579f6
Stefan Beier <stefan.beier@meinfernbus.de>
2015-10-29 13:54:03 +0100

    Init commit.

---
+++ src/Command/WandelMenuCommand.php
@@ -0,0 +1,135 @@
+<?php
+/**
+ * @author Stefan Beier <stefan.beier@meinfernbus.de>
+ */
+namespace MFB\SlackReactor\Command;
+
+use Cilex\Command\Command;
+use React\EventLoop\Factory;
+use React\Http\Request;
+use React\Http\Response;
+use React\Socket\Server;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Yaml\Yaml;
+
+/**
+ * Class WandelMenuCommand
+ */
+class WandelMenuCommand extends Command
+{
+
+    const COMMAND_NAME = 'stefan:slack:wandel';
+
+    /**
+     * Post wandel menu only every 5 minutes.
+     */
+    const SPAM_FILTER = 300;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function configure()
+    {
+        $this->setName(self::COMMAND_NAME);
+        $this->setDefinition([new InputOption('--config', null, InputOption::VALUE_REQUIRED, 'Path to configuration file'),]);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function initialize(InputInterface $input, OutputInterface $output)
+    {
+        if (!$configFilePath = realpath($input->getOption('config'))) {
+            throw new \RuntimeException('Configuration file path is invalid.');
+        }
+
+        $container = $this->getContainer();
+
+        $settings = Yaml::parse(file_get_contents($configFilePath))['settings'];
+
+        $container['settings'] = $settings;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $loop            = Factory::create();
+        $socket          = new Server($loop);
+        $container       = $this->getContainer();
+        $wandelSettings  = $container['settings']['servers']['wandel'];
+        $http            = new \React\Http\Server($socket);
+        $lastRequested   = 0;
+        $spamWarningSent = false;
+
+        $app = function (Request $request, Response $response) use ($wandelSettings, &$lastRequested, &$spamWarningSent) {
+            $request->on('data', function ($data) use ($request, $response, $wandelSettings, &$lastRequested, &$spamWarningSent) {
+
+                parse_str($data, $postParams);
+
+                // If there is nothing, do nothing.
+                if ($data == '' || $postParams['token'] != $wandelSettings['slack_token']) {
+                    $response->writeHead(404);
+                    $response->end();
+
+                    return;
+                }
+
+                // Spam filter and send spam warning text.
+                if (time() - $lastRequested < WandelMenuCommand::SPAM_FILTER) {
+
+                    if (!$spamWarningSent) {
+                        $payload = json_encode(['text' => 'Hey, do not spam the WandelBot!']);
+                        $response->writeHead(200);
+                        $response->end($payload);
+                        $spamWarningSent = true;
+
+                        return;
+                    }
+
+                    return;
+                }
+
+                // Check valid url format.
+                $wandelMenuUrl = null;
+                foreach ($wandelSettings['menu_1'] as $menuFormat) {
+                    $wandelMenuUrl = sprintf($menuFormat, date('W'));
+                    if (file_get_contents($wandelMenuUrl)) {
+                        break;
+                    }
+                }
+
+                // No menu? well then there is nothing here.
+                if ($wandelMenuUrl == null) {
+                    $response->writeHead(404);
+                    $response->end();
+
+                    return;
+                }
+
+                $responseText    = $wandelMenuUrl . ' - Brought you by horriblesolutions.com';
+                $payload         = json_encode(['text' => $responseText]);
+                $spamWarningSent = false;
+                $lastRequested   = time();
+
+                $response->writeHead(200);
+                $response->end($payload);
+
+            });
+
+        };
+
+        $http->on('request', $app);
+
+        $host = $wandelSettings['host'];
+        $port = $wandelSettings['port'];
+
+        $socket->listen($port, $host);
+        $output->writeln(sprintf('Server started on %s:%s', $host, $port));
+        $loop->run();
+    }
+
+}
